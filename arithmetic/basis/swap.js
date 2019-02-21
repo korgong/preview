aae0ff1dacbfaa4d9f178d280d1c93aacf7cc601
gongke <gongke@baidu.com>
2019-02-21 10:49:09 +0800

    添加标注

--- arithmetic/basis/swap.js
+++ arithmetic/basis/swap.js
@@ -6,10 +6,11 @@ function swapUseTemp(a, b) {
 }
 function swapUseJs(a, b) {
     b = [a, a = b][0];
-    debugger
     console.log(a, b);
 }
 
+// c实现，适用于数字类型的交换，上面两种适用于任何类型
+// 注意这里的=不是数学意义上的，而是赋值操作。
 function swapUseC(a, b) {
     a = a + b;
     b = a - b;
