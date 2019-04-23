58b23dc6c672d66f41dbf1f63a3cca72eea3d7c2
gongke <gongke@baidu.com>
2019-04-23 17:44:04 +0800

    debounce

--- collection/throttle.js
+++ collection/throttle.js
@@ -1,3 +1,4 @@
+// throttle 节流
 // 使用方法存储id
 // function throttle(method, context) {
 //     clearInterval(method.timeout);
@@ -39,3 +40,39 @@ function throttle(method, delay) {
 // fn(2);
 // fn(3);
 
+
+// debounce 防抖
+function debounce(method, delay, mustRunTime) {
+    let timer = null;
+    let startTime = null;
+    return function () {
+        let that = this;
+        let args = arguments;
+        let currentTime = new Date().getTime();
+        clearTimeout(timer);
+        if (!startTime) {
+            startTime = currentTime;
+        } else if (currentTime - startTime >= mustRunTime) {
+            method.apply(that, args);
+            startTime = currentTime;
+        } else {
+            timer = setTimeout(function () {
+                method.apply(that, args);
+            }, delay)
+        }
+    }
+}
+
+// 第一次和第二次小于最小执行时间，都被clear。但是初始时间一直被记录。
+// 第三次大于最小间隔，执行。第四次小于最小间隔。但是没有新的执行动作。40ms后就会执行。
+let fn = debounce(method, 50, 30);
+fn(1)
+setTimeout(function () {
+    fn(2);
+}, 20);
+setTimeout(function () {
+    fn(3);
+}, 30);
+setTimeout(function () {
+    fn(4);
+}, 40);
