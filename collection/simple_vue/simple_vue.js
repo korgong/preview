3cccb15ee02775addf2114603f43b0cebd5bbb56
gongke <gongke@baidu.com>
2019-05-08 16:34:49 +0800

    add json2node for simple vue

--- collection/simple_vue/simple_vue.js
+++ collection/simple_vue/simple_vue.js
@@ -1,7 +1,6 @@
 // 创建viewModel
 // 提取解析指令（订阅数据变化，绑定视图更新函数、初始化视图）
-// todo 1、defineProperty只提取了最外层的key值  2、创建碎片dom只取第一个孩子节点，如果指令在孩子节点的子节点无法识别
-// todo 3、碎片dom如何工作的
+// todo 1、defineProperty只提取了最外层的key值
 function observe(vm, data) {
     let deep = new Deep();
     Object.keys(data).forEach(key => {
@@ -95,23 +94,109 @@ function compile(vm, node) {
 
 }
 
-function nodeToFragment(vm, app) {
-    let fragment = document.createDocumentFragment();
-    let child;
-    while (child = app.firstChild) {
-        compile(vm, child);
-        fragment.appendChild(child);
+function updateAttrInJson(rootJson, virtualDom) {
+    for (let attr in rootJson) {
+        if (attr === 'attributes') {
+            if (virtualDom[attr]) {
+                rootJson[attr] = [].slice.call(virtualDom[attr]).map(attribute => {
+                    return {
+                        [attribute.name]: attribute.nodeValue
+                    }
+                });
+            }
+        } else if (attr === 'childNodes') {
+            rootJson.childNodes = [];
+        } else {
+            // 包括nodeName，nodeType
+            rootJson[attr] = virtualDom[attr];
+        }
     }
-    return fragment;
+}
+
+function getJson(virtualDom) {
+    // 创建父节点的json
+    let rootJson = {
+        nodeType: 1,  // 文本节点
+        nodeName: 'DIV',  // 文本节点#text
+        attributes: [],
+        nodeValue: null,  // 文本节点是字符串
+        childNodes: [],
+    };
+    // 更新json属性
+    updateAttrInJson(rootJson, virtualDom);
+    // 得到父节点子节点，将子节点放入json的childNodes中。
+    // 同时子节点的childNodes的个数不为空，对子节点进行递归
+    let childNodes = virtualDom.childNodes;
+    [].slice.call(childNodes).forEach(child => {
+        let json = {
+            nodeType: 1,  // 文本节点
+            nodeName: 'DIV',  // 文本节点#text
+            attributes: [],
+            nodeValue: null,  // 文本节点是字符串
+            childNodes: [],
+        };
+        // 更新json属性
+        updateAttrInJson(json, child);
+        // 判断子节点的类型，确定是否递归
+        if (!child.childNodes.length) {
+            rootJson.childNodes.push(json);
+        } else {
+            rootJson.childNodes.push(getJson(child));
+        }
+    });
+    return rootJson;
+}
+
+
+function updateAttributes(json, node) {
+    if (json.attributes.length) {
+        json.attributes.forEach(attribute => {
+            Object.keys(attribute).forEach(key => {
+                node.setAttribute(key, attribute[key]);
+            })
+        })
+    }
+}
+
+
+function json2node(json, vm) {
+    // 获取父节点，将子节点加入父节点。子节点如果还有元素，递归执行
+    let rootNode = document.createElement(json.nodeName);
+    updateAttributes(json, rootNode);
+    json.childNodes.forEach(child => {
+        if (!child.childNodes.length) {
+            // 无子节点json
+            let node;
+            if (child.nodeType === 3) {
+                node = document.createTextNode(child.nodeValue);
+            } else {
+                // img...
+                node = document.createElement(child.nodeName);
+                updateAttributes(child, node);
+            }
+            compile(vm, node);
+            rootNode.appendChild(node);
+        } else {
+            rootNode.appendChild(json2node(child, vm));
+        }
+    });
+    return rootNode;
+}
+
+function nodeToFragment(vm, virtualDom) {
+    let json = getJson(virtualDom);
+    let dom = json2node(json, vm);
+    return dom;
 }
 
 function Vue(option) {
     this.el = option.el;
     this.data = option.data;
     observe(this, option.data);
-    let app = document.getElementById('app');
-    let dom = nodeToFragment(this, app);
-    app.appendChild(dom);
+    let virtualDom = document.getElementById(this.el);
+    let dom = nodeToFragment(this, virtualDom);
+    virtualDom.parentNode.removeChild(virtualDom);
+    document.body.appendChild(dom);
 }
 
 let vm = new Vue({
